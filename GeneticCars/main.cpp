53c29fbcf6ffebbd060901000842bddb2f75018e
ambro01 <rafalambro@gmail.com>
2017-01-12 00:12:50 +0100

    Solve problem with wheels position.

--- GeneticCars/main.cpp
+++ GeneticCars/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	// Krotki opis dzialania - PopulationManager
 
-	const int size = 4; // rozmiar populacji 
+	const int size = 2; // rozmiar populacji 
 	const float rate = 0.0; // wspolczynnik mutacji od 0.0 - 1.0 - im wiekszy tym wieksza sznasa na mutacje, przy 0 nie ma mutacji
 	PopulationManager *pm = new PopulationManager(size, rate); // robisz managera do populacji
 	std::vector<Car> inPop = pm->generateInitialPopulation(); // generujesz pierwsza populacje ( z randomowymi wartosciami w carach)
@@ -47,9 +47,9 @@ int main()
 		for (int j = 0; j < NR_OF_POINTS_IN_BODYSHAPE; ++j) {
 			std::cout << '[' << i << ']' << "  " << '[' << j << ']' << newPop[i].getChromosome().getBodyShape().getShapePoints()[j].getX() << "  " << newPop[i].getChromosome().getBodyShape().getShapePoints()[j].getY() << std::endl;
 		}
-		std::cout << "Kolo3" << newPop[i].getChromosome().getWheels()[0].getShapePoint().getX() << "  " << newPop[i].getChromosome().getWheels()[0].getShapePoint().getY() << std::endl;
-		std::cout << "Kolo4" << newPop[i].getChromosome().getWheels()[1].getShapePoint().getX() << "  " << newPop[i].getChromosome().getWheels()[1].getShapePoint().getY() << std::endl;
-		std::cout << std::endl;
+		//std::cout << "Kolo3" << newPop[i].getChromosome().getWheels()[0].getShapePoint().getX() << "  " << newPop[i].getChromosome().getWheels()[0].getShapePoint().getY() << std::endl;
+		//std::cout << "Kolo4" << newPop[i].getChromosome().getWheels()[1].getShapePoint().getX() << "  " << newPop[i].getChromosome().getWheels()[1].getShapePoint().getY() << std::endl;
+		//std::cout << std::endl;
 	}
 
 	boost::shared_ptr<Simulation> simulation(new Simulation());
