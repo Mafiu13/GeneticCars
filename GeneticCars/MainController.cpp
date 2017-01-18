60e1f46cf419872992e27221571c44aba01d2002
ambro01 <rafalambro@gmail.com>
2017-01-18 11:01:21 +0100

    Make MainController class

--- GeneticCars/MainController.cpp
+++ GeneticCars/MainController.cpp
@@ -11,26 +11,87 @@ MainController::~MainController()
 {
 }
 
+PPhysSimulation MainController::getSimualation()
+{
+	return simulation;
+}
+
+PPopulationManager MainController::getPopulationManager()
+{
+	return populationManager;
+}
+
+PAppWindow MainController::getWindow()
+{
+	return window;
+}
+
+void MainController::setSimulation(PPhysSimulation s)
+{
+	simulation = s;
+}
+
+void MainController::setPopulationManager(PPopulationManager p)
+{
+	populationManager = p;
+}
+
+void MainController::setWindow(PAppWindow w)
+{
+	window = w;
+}
+
 std::vector<CarSh> MainController::convertPhysCarToCar(std::vector<Car> cars)
 {
-	std::vector<CarSh> cars_ph;
+	std::vector<CarSh> physCars;
 	std::vector<WheelSh> w;
 	WheelSh wheel;
 	PhysBodyShape shape;
 	for (int i = 0; i < cars.size(); ++i) {
 		w.clear();
-		cars_ph.push_back(CarSh(new PhysCar()));
-		cars_ph[i]->setBodyShape(boost::shared_ptr<PhysBodyShape>(new PhysBodyShape()));
-		cars_ph[i]->getBodyShape()->setDensity(cars[i].getChromosome().getBodyShape().getDensity());
-		cars_ph[i]->getBodyShape()->setVert(cars[i].getChromosome().getBodyShape().getShapePoints());
+		physCars.push_back(boost::make_shared<PhysCar>());
+		physCars[i]->setBodyShape(boost::make_shared<PhysBodyShape>());
+		physCars[i]->getBodyShape()->setDensity(cars[i].getChromosome().getBodyShape().getDensity());
+		physCars[i]->getBodyShape()->setVert(cars[i].getChromosome().getBodyShape().getShapePoints());
 		for (Wheel wh : cars[i].getChromosome().getWheels()) {
-			wheel = WheelSh(new PhysWheel());
+			wheel = boost::make_shared<PhysWheel>();
 			wheel->setDensity(wh.getDensity());
 			wheel->setRadius(wh.getRadius());
 			wheel->setJointPoint(wh.getShapePoint());
 			w.push_back(wheel);
 		}
-		cars_ph[i]->setWheels(w);
+		physCars[i]->setWheels(w);
 	}
-	return cars_ph;
+	return physCars;
+}
+
+std::vector<float> MainController::getDistances()
+{
+	std::vector<float> out;
+	for (CarSh car: simulation->getPopulation()->getCars()) {
+		out.push_back(car->getBodyShape()->getBody()->GetPosition().x);
+	}
+	return out;
+}
+
+void MainController::simulatePupulation()
+{
+	simulation->updateSimulation();
+	window->drawAll(simulation->getDrawing());
+	window->getWindow().display();
+	window->getView().setCenter(sf::Vector2f(simulation->getPopulation()->getTheFastestX() * SCALE, 300));
+	window->setViewToWindow();
+}
+
+bool MainController::finishCheck()
+{
+	bool finish = false;
+	if (simulation->getPopulation()->getTheFastestX() > 0) {
+		finish = true;
+	}
+	for (CarSh car : simulation->getPopulation()->getCars()) {
+		if (car->getBodyShape()->getBody()->GetLinearVelocity().x > 0.01 || abs(car->getBodyShape()->getBody()->GetLinearVelocity().y) > 0.01)
+			finish = false;
+	}
+	return finish;
 }
--- GeneticCars/MainController.h
+++ GeneticCars/MainController.h
@@ -4,15 +4,29 @@
 #include <boost/shared_ptr.hpp>
 #include "PhysSimulation.h"
 #include "PopulationManager.h"
+#include "AppWindow.h"
 
+typedef boost::shared_ptr <PhysSimulation> PPhysSimulation;
+typedef boost::shared_ptr <PopulationManager> PPopulationManager;
+typedef boost::shared_ptr <AppWindow> PAppWindow;
 class MainController
 {
-	boost::shared_ptr<PhysSimulation> simulation;
-	boost::shared_ptr<PopulationManager> manager;
+	PPhysSimulation simulation;
+	PPopulationManager populationManager;
+	PAppWindow window;
 public:
 	MainController();
 	~MainController();
+	PPhysSimulation getSimualation();
+	PPopulationManager getPopulationManager();
+	PAppWindow getWindow();
+	void setSimulation(PPhysSimulation);
+	void setPopulationManager(PPopulationManager);
+	void setWindow(PAppWindow);
 	std::vector<CarSh> convertPhysCarToCar(std::vector<Car>);
+	std::vector<float> getDistances();
+	void simulatePupulation();
+	bool finishCheck();
 };
 
 #endif
