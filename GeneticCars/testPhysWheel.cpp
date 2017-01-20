ee666229d95bcfe38d872eae1b283b011dfe450e
ambro01 <rafalambro@gmail.com>
2017-01-20 01:41:20 +0100

    Correct tests

--- GeneticCars/testPhysWheel.cpp
+++ GeneticCars/testPhysWheel.cpp
@@ -6,8 +6,8 @@
 BOOST_AUTO_TEST_CASE(TestPhysWheel_Attributes)
 {
 	PPhysWheel wheel = boost::make_shared<PhysWheel>();
-	wheel->setDensity(0.5);
-	wheel->setRadius(0.4);
-	BOOST_CHECK_EQUAL(wheel->getDensity(), 0.5);
-	BOOST_CHECK_EQUAL(wheel->getRadius(), 0.4);
+	wheel->setDensity(0.5f);
+	wheel->setRadius(0.4f);
+	BOOST_CHECK_EQUAL(wheel->getDensity(), 0.5f);
+	BOOST_CHECK_EQUAL(wheel->getRadius(), 0.4f);
 }
